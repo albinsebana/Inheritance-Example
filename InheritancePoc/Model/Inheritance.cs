5c2c54049622ad7222a140ee41b263b3d2f40704
Albin Kurian <Albin.Kurian@asl.mum.sst>
2023-08-11 22:02:59 +0530

    Inheritance Exapmle  poc

---
+++ InheritancePoc/Model/Inheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InheritancePoc.Model
+{
+    internal class Animal
+    {
+        public string Name { get ; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} is eating.");
+        }
+    }
+
+    // Derived class (subclass)
+    class Dog : Animal//Dog is inheriting from the class Animal
+    {
+        public void Bark()//Dog class can define its own unique properties and methods.
+        {
+            Console.WriteLine($"{Name} is barking.");
+        }
+    }
+
+}
+
---
+++ InheritancePoc/Program.cs
@@ -0,0 +1,31 @@
+﻿using InheritancePoc.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePoc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Create an instance of the Dog class
+            Dog myDog = new Dog();
+            myDog.Name = "Puppy";//Dog class inherits the Name property and
+
+            // Inherited method from the base class-->Dog class inherits the Eat()
+            myDog.Eat();
+
+            // Method specific to the derived class
+            myDog.Bark();
+        }
+    }
+}
+
+//If you don't want other classes to inherit from a class, use the sealed keyword
+
+//sealed class Vehicle 
+//{
+//    ...
+//}
